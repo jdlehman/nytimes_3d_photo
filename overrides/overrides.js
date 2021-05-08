7d272616c197e3259a035a159671964d7e579dbe
Jonathan <jonathan.lehman91@gmail.com>
2021-05-07 18:42:18 -0700

    Use recipe page

--- overrides/overrides.js
+++ overrides/overrides.js
@@ -1,47 +1,52 @@
-function getParameterByName(name) {
-    var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
-    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
-}
+(function(){
+  function getParameterByName(name) {
+      var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+      return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+  }
 
-const photo3DUrls = [
-  'https://www.facebook.com/Dino3DPhotos/posts/290430298542834',
-  'https://www.facebook.com/Dino3DPhotos/posts/290429878542876',
-  'https://www.facebook.com/Dino3DPhotos/posts/285515049034359',
-  'https://www.facebook.com/Dino3DPhotos/posts/290429005209630',
-  'https://www.facebook.com/Dino3DPhotos/posts/290429558542908',
-  'https://www.facebook.com/Dino3DPhotos/posts/285515935700937',
-];
-let main3DPhotoUrl = 'https://www.facebook.com/Dino3DPhotos/posts/290427008543163';
-const mainPhotoCSS = 'figure.css-1fwiylb';
-const smallPhotoCSS = 'figure.css-1ag53q4';
+  function replacePhotoWith3DPhoto(node, url, width) {
+    node.innerHTML = `
+    <div class="fb-post"
+        data-href="${url}"
+        data-width="${width}"></div>
+        `;
+  }
 
-const overrideUrl = getParameterByName("overrideurl");
-const useRandom = getParameterByName("random");
-if (overrideUrl) {
-  main3DPhotoUrl = overrideUrl;
-} else if (useRandom) {
-  photo3DUrls.push(main3DPhotoUrl);
-  const index = Math.floor(Math.random() * photo3DUrls.length);
-  main3DPhotoUrl = photo3DUrls[index];
-}
+  const articlePhoto3DUrls = [
+    null,
+    null,
+    'https://www.facebook.com/michael.cohen.12764/posts/10159452170718699',
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ];
+  let main3DPhotoUrl = 'https://www.facebook.com/michael.cohen.12764/posts/10159452170718699';
+  const mainPhotoCSS = '.css-bsn42l';
+  const articlePhotoCSS = '.css-zjzyr8';
 
-function replacePhotoWith3DPhoto(node, url, width) {
-  node.innerHTML = `
-  <div class="fb-post"
-      data-href="${url}"
-      data-width="${width}"></div>
-      `;
-}
+  const overrideUrl = getParameterByName("overrideurl");
+  const all = getParameterByName("all");
+  if (overrideUrl) {
+    main3DPhotoUrl = overrideUrl;
+  } else if (all) {
+    const smallPhotos = document.querySelectorAll(articlePhotoCSS);
+    let i = 0;
+    smallPhotos.forEach(function(node) {
+      console.log(node)
+      if (articlePhoto3DUrls[i] != null) {
+        replacePhotoWith3DPhoto(node, articlePhoto3DUrls[i], 720);
+      }
+      i++;
+    });
+  }
 
-const mainPhoto = document.querySelector(mainPhotoCSS);
-replacePhotoWith3DPhoto(mainPhoto, main3DPhotoUrl, 1000);
-
-// uncomment to replace other photos with 3D photos on page
-// const smallPhotos = document.querySelectorAll(smallPhotoCSS);
-// let i = 0;
-// smallPhotos.forEach(function(node) {
-//   if (photo3DUrls[i]) {
-//     replacePhotoWith3DPhoto(node, photo3DUrls[i], 400);
-//   }
-//   i++;
-// });
+  const mainPhoto = document.querySelector(mainPhotoCSS);
+  replacePhotoWith3DPhoto(mainPhoto, main3DPhotoUrl, 1000);
+}());
